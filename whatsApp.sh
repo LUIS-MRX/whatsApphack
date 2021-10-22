20b91f619e66e3e8277aa1a49f1a1ed932db7942
José Luis urbano <82736353+LUIS-MRX@users.noreply.github.com>
2021-10-21 23:35:58 -0300

    Add files via upload
    WhatsApp troleo

---
+++ whatsApp.sh
@@ -0,0 +1,68 @@
+/#!bin/bash
+#by LUIS_MRX
+#telegram @LUIS_MRX
+#GRUPO TELEGRAM @LUIS_MRX1
+#SCRIP DE BROMAS JAJAJAJ
+#ESTO ES SIMPLE VOY AORENDIENDO
+#2021
+#YA TU SABES 
+
+line="━━━━━━━━━━━━━━━━━━━━━━"
+clear
+echo "$line"
+echo -e "\e[7;32mPANEL DE HACK LUIS MRX\e[0m"
+echo "$line"
+sleep 1
+echo -e "\e[1;32;41m"
+echo "By LUIS MRX"
+echo -e "\e[1;30;40m"
+echo ""
+echo ""
+sleep 1
+echo -e "\e[1;32;40m"
+echo "Bienvenido :)"
+echo "Hackea Whatsapp "
+echo ""
+echo -e "\e[1;32m"
+echo "Presione ENTER para comenzar "
+read ENTER
+sleep 1
+echo ""
+echo -e "\e[1;33;40m"
+echo "Escribe Su numero de la victima: "
+read ENTER
+echo -e "\e[1;34;40m"
+echo ""
+echo "Buscando numero  de la Victima....."
+sleep 3
+echo -e "\e[1;32;40m"
+echo ""
+echo "Numero encontrado :D"
+sleep 3
+echo ""
+echo -e "\e[1;37;40m"
+echo "Presione ENTER para comenzar el ataque "
+read ENTER
+sleep 3
+echo -e "\e[34mAHORA SE VIENE LO SHIDOO XD XD XD XD\e[0m"
+sleep 1
+echo -e "\e[1;31;40m"
+echo ""
+echo ""
+echo -e "\e[1;31;42m"
+echo  ""
+echo "⚠️ ⚠️ SU DISPOSITIVO HA SIDO INFECTADO, GRACIAS!!!!! ⚠️ ⚠️  <3 🥰"
+sleep 2
+echo ""
+echo "🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮"
+echo "🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮"
+echo "" 
+sleep 2
+echo -e "\e[1;32;41m"
+echo ""
+yes 💣  💀 ⚠️@LUIS_MRX⚠️  💀 ⚠️@LUIS_MRX⚠️ 💀 👹OBTENIENDO INFORMACION👹 💀 🤑DATOS ROBADOS🤑 💀 👥DESCARGANDO CONTACTOS👥 💀 🚀CONTRASEÑAS GUARDADAS🚀 💀 🦠ENCRIPTANDO ARCHIVOS🦠 💀 📲OBTENIENDO FOTOS📲 💀 📱OBTENIENDO VIDEOS📱 💀 🤖CAMBIANDO CONTRASEÑAS🤖 💀 👁️ENVIANDO DOCUMENTOS👁️ 💀 🥰GRACIAS🥰💀☠️🔥🤮🤮🤮🤮🤮 🔥su dispositivo ha sido infectado ☠️ 🥵🥵🥵🥵🥵 🔥su dispositivo ha sido infectado🤮☠️👾  |head -n 10000000000
+sleep 3
+# FIN DE SCRITG 
+#NO COPIES EL CÓDIGO 😤😤😤😤😤😤😤
+
+
